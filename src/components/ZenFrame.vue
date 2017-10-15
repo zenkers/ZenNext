bc9d89beafdc6962a0a9a1e4a641a026f9ad6b62
Zenkers <dpswanson2010@gmail.com>
2017-10-14 23:12:53 -0400

    Extracted frames from ZenNext into it's own component ZenFrame.

---
+++ src/components/ZenFrame.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="zen-frame">
+        <div class=" zen-content">
+            <h2>{{ title }}</h2>
+            <p>{{ content }}</p>
+        </div>
+
+        <div class="zen-button--container">
+            <button class="zen-button zen-button--next" 
+                    @click="nextFrame" 
+                    v-if="index === length-1">
+                Next
+            </button>
+            <span v-else-if="index === 0">
+                <button class="zen-button--half zen-button--previous" 
+                        @click="previousFrame">
+                    Previous
+                </button>
+                <button class="zen-button--half zen-button--close" 
+                        @click="closeModal">
+                    Close
+                </button>
+            </span>
+            <span v-else>
+                <button class="zen-button--half zen-button--previous" 
+                        @click="previousFrame">
+                    Previous
+                </button>
+                <button class="zen-button--half zen-button--next" 
+                        @click="nextFrame">
+                    Next
+                </button>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['index', 'title', 'content', 'length', 'visible'],
+        methods: {
+            // Recursively checks for @classString in node.classList
+            // If not found, checks node.offsetParent
+            findParent(node, classString) {
+                if ( node.classList ) {
+                    if (node.classList.contains(classString)) {
+                        return node
+                    } else {
+                        return this.findParent(node.offsetParent, classString)
+                    }
+                } else {
+                    return false
+                }
+            },
+            // Hides current frame by adding class close-frame
+            nextFrame(e) {
+                let parent = this.findParent(e.target, 'zen-frame')
+                
+                if ( parent ) {
+                    parent.classList.add('close-frame')
+                } else {
+                    console.log('Error: No such parent. (nextFrame)')
+                }  
+            },
+            // Shows previous frame by removing class 'close-frame'
+            previousFrame(e) {
+                let previousFrame = this.findParent(e.target, 'zen-frame')
+
+                if ( parent ) {
+                    previousFrame.nextElementSibling.classList.remove('close-frame')
+                } else {
+                    console.log('Error: No such parent. (previousFrame)')
+                }
+            },
+            // Shows all frames by removing class 'close-frame'
+            resetTabs() {
+                let closedFrames = document.querySelectorAll('.zen-frame.close-frame')
+                closedFrames.forEach( (frame) => {
+                    frame.classList.remove('close-frame')
+                })
+            },
+            closeModal() {
+                this.resetTabs()
+                this.$emit('visible', false)
+            }
+        }
+    }
+</script>
+
+
+<style lang="scss">
+    .zen-frame {
+        width: 100%;
+        height: 100%;
+        background-color: white;
+        border: 1px solid black;
+        border-radius: 10px;
+        overflow: hidden;
+        padding: 20px;
+        position: absolute;
+
+        &.close-frame {
+            display: none;
+        }
+
+        .zen-content {
+            display: block;
+        }
+
+        .zen-button--container {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+
+            .zen-button {
+                width: 100%;
+                padding: 10px;
+                color: white;
+
+                &--half {
+                    width: 50%;
+                    padding: 10px;
+                    float: left;
+                    color: white;
+                }
+
+                &--next {
+                    background-color: green;
+                }
+
+                &--previous {
+                    background-color: blue;
+                }
+
+                &--close {
+                    background-color: red;
+                }
+            }
+        }
+    }
+</style>
